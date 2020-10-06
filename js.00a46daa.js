200fb0c343f13cc56ab82ab4d3f130c49ea27f37
dd8888 <daviddiazh97@gmail.com>
2020-10-06 21:31:42 +0100

    Updates

--- js.00a46daa.js
+++ js.00a46daa.js
@@ -129,15 +129,42 @@ boton2.onclick = getRandomDado2;
 boton3.onclick = getRandomDado3;
 
 function getRandomDado1() {
-  dado1.textContent = Math.ceil(Math.random() * 6);
+  var number = Math.ceil(Math.random() * 6);
+  dado1.style.backgroundImage = "url('" + getImage(number) + "')";
 }
 
 function getRandomDado2() {
-  dado2.textContent = Math.ceil(Math.random() * 6);
+  var number = Math.ceil(Math.random() * 6);
+  dado2.style.backgroundImage = "url('" + getImage(number) + "')";
 }
 
 function getRandomDado3() {
-  dado3.textContent = Math.ceil(Math.random() * 6);
+  var number = Math.ceil(Math.random() * 6);
+  dado3.style.backgroundImage = "url('" + getImage(number) + "')";
+}
+
+function getImage(number) {
+  switch (number) {
+    case 1:
+      return "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Dice-1-b.svg/768px-Dice-1-b.svg.png";
+
+    case 2:
+      return "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Dice-2-b.svg/600px-Dice-2-b.svg.png";
+
+    case 3:
+      return "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Dice-3-b.svg/1024px-Dice-3-b.svg.png";
+
+    case 4:
+      return "https://upload.wikimedia.org/wikipedia/commons/f/fd/Dice-4-b.svg";
+
+    case 5:
+      return "https://tr.rbxcdn.com/514984bf832371b30a63768669743683/420/420/Decal/Png";
+
+    case 6:
+      return "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Dice-6-b.svg/768px-Dice-6-b.svg.png";
+
+    default:
+  }
 }
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -167,7 +194,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63571" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64285" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
